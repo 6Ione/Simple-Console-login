052e7e889b5fd2eaa375cfa40b858f5f2bcf302b
C# <80015177+6Ione@users.noreply.github.com>
2021-03-03 18:56:43 -0700

    Add files via upload

---
+++ Simple_Console_Login/Simple_Console_Login/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Threading;
+
+namespace Simple_Console_Login
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Red; // Changes the console color, Delete if you want.
+            WebClient wc = new WebClient();
+            string Whitelisted = wc.DownloadString("https://pastebin.com/raw/mSh84DYc"); // Put your URL that has the logins here.
+
+            Console.Write("Enter Login: ");
+            string login = Console.ReadLine();
+
+            if (Whitelisted.Contains(login)) // checks if user input is whitelisted.
+            {
+                Console.Clear();
+                Console.WriteLine("Logged in."); // enter your code here.
+
+            }
+
+            else
+            {
+                Console.WriteLine("Enter correct login.");
+            }
+            Thread.Sleep(-1); // keep program running
+        }
+    }
+}
